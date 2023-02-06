ba50e3a157dc1e5817bcd10a43df9227ff9be5f2
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-02-06 03:26:44 +0100

    Done with the carousel

--- JS/scroll.js
+++ JS/scroll.js
@@ -47,31 +47,76 @@ if (window.matchMedia("(min-width: 768px)").matches) {
 
 
 
-  // const imgWrapper = document.getElementById("name");
-  //   imgWrapper.addEventListener("mouseover", (e) => {  
-  //   const imgWrapperChildNodes = imgWrapper.childNodes
-
-  //   const fadeInBtn = imgWrapperChildNodes[7].style.color = "blue"
-  //   const fadeInText = imgWrapperChildNodes[5].style.color = "red" 
-
-
-  //   // fadeInBtn.style.color = "red"
-  //   console.log()
-  //   e.preventDefault()
-   
-  // })
-
-
-  // imgWrapper.addEventListener("mouseout", (e) => {
-  //   const imgWrapperChildNodes = imgWrapper.childNodes
-
-  //   const fadeInBtn = imgWrapperChildNodes[7].style.color = "blue"
-  //   const fadeInText = imgWrapperChildNodes[5].style.color = "white" 
-  
-  
-  //   // fadeInBtn.style.color = "red"
-  //   console.log()
-  //   e.preventDefault()
       
-  // })
+    
+    let imgWrapper1 = document.getElementById("name-0"),
+        imgWrapper2 = document.getElementById('name-1'),
+        imgWrapper3 = document.getElementById('name-2'),
+        imgWrapper4 = document.getElementById('name-3'),
+        imgWrapper5 = document.getElementById('name-4')
+    
+
+        ///// 1  ///////
+        
+    imgWrapper1.addEventListener("mouseover", (e) => {  
+    imgWrapper1.childNodes[5].style.opacity = 0
+    e.preventDefault()
+  })
+
+  imgWrapper1.addEventListener("mouseout", (e) => {  
+    imgWrapper1.childNodes[5].style.opacity = 1
+    e.preventDefault()
+  })
+
+
+  //////  2 ////////
+
+  imgWrapper2.addEventListener("mouseover", (e) => {  
+    console.log(imgWrapper2.childNodes)
+    imgWrapper2.childNodes[5].style.opacity = 0
+    e.preventDefault()
+  })
+
+  imgWrapper2.addEventListener("mouseout", (e) => {  
+    imgWrapper2.childNodes[5].style.opacity = 1
+    e.preventDefault()
+  })
+
+
+  ////  3 ////////
+
+  imgWrapper3.addEventListener("mouseover", (e) => {  
+    imgWrapper3.childNodes[1].childNodes[5].style.opacity = 0
+    e.preventDefault()
+  })
+
+  imgWrapper3.addEventListener("mouseout", (e) => {  
+    imgWrapper3.childNodes[1].childNodes[5].style.opacity = 1
+    e.preventDefault()
+  })
+
+
+  ////  4 ////////
+
+  imgWrapper4.addEventListener("mouseover", (e) => {  
+    imgWrapper4.childNodes[1].childNodes[5].style.opacity = 0
+    e.preventDefault()
+  })
+
+  imgWrapper4.addEventListener("mouseout", (e) => {  
+    imgWrapper4.childNodes[1].childNodes[5].style.opacity = 1
+    e.preventDefault()
+  })
+
+
+
+
+
+
+
+
+
+
+
+
 
