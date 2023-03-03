2d45ea5c967929c8e53852b5a83c01ee9f8b4d36
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-03-03 04:40:09 +0100

    Working on the side bar

--- JS/settings.js
+++ JS/settings.js
@@ -1,13 +1,35 @@
 const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
+const list = document.getElementById('list')
+const button = document.getElementById('btn')
+const btnSideBarMode = document.getElementsByClassName('sidebar-mode')
+console.log(sidebar.classList)
 
-document.getElementById('btn').addEventListener('click', function(e) {
+button.addEventListener('click', function(e) {
     e.preventDefault()
-   sidebar.classList.toggle('sidebar_small');
-   mainContent.classList.toggle('main-content_large')
+    
+    if(e.target.classList.contains('sidebar-mode')){
+        console.log(button.classList )
+        button.classList.remove('sidebar-mode')
+        
+        sidebar.classList.toggle('sidebar_small');
+        list.style.display = "none"
+    }
+    if(!e.target.classList.contains('sidebar-mode')) {
+        console.log(button.classList)
+        button.classList.add('sidebar-mode')
+
+        list.style.display = "block"
+        mainContent.classList.toggle('main-content_large')
+        
+    }
+
 })
-// document.querySelector('button').onclick = function () {
-//     console.log('I got clicked!')
-//   sidebar.classList.toggle('sidebar_small');
-//   mainContent.classList.toggle('main-content_large')
-// }
+
+// btnSideBarMode.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     sidebar.classList.toggle('sidebar_small');
+//     list.style.display = "none"
+//     mainContent.classList.toggle('main-content_large')
+// })
+
